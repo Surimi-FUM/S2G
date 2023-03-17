bba3914571c8283e589e10b4bee06e70042b6166
Sen_430 <senkoji.k.aa@m.titech.ac.jp>
2023-03-17 11:50:21 +0900

    プロジェクト ファイルを追加します。

---
+++ S2G/Main.cpp
@@ -0,0 +1,67 @@
+﻿/*
+* 最初のゲーム開発
+* 基本プログラムはC++で記述
+* OpenSiv3Dはグラフィック処理のみとする
+*/
+# include <Siv3D.hpp> // OpenSiv3D v0.6.6
+#include "map_class.h"
+
+void Main()
+{
+	// ウィンドウを 640x480 にリサイズする
+	Window::Resize(640, 480);
+
+	std::string csv_path = "maps/csv/map1.csv";
+
+	Map map = Map(csv_path);
+
+	bool flag_show = false;
+
+	map.DebugConsole();
+
+	while (System::Update())
+	{
+		if (map.CheckClear() || map.CheckOver())
+			break;
+
+		if (KeyLeft.down())
+			map.MovePlayer(1, "Left");
+
+		if (KeyRight.down())
+			map.MovePlayer(1, "Right");
+
+		if (KeyUp.down())
+			map.MovePlayer(1, "Up");
+
+		if (KeyDown.down())
+			map.MovePlayer(1, "Down");
+	}
+}
+
+//
+// - Debug ビルド: プログラムの最適化を減らす代わりに、エラーやクラッシュ時に詳細な情報を得られます。
+//
+// - Release ビルド: 最大限の最適化でビルドします。
+//
+// - [デバッグ] メニュー → [デバッグの開始] でプログラムを実行すると、[出力] ウィンドウに詳細なログが表示され、エラーの原因を探せます。
+//
+// - Visual Studio を更新した直後は、プログラムのリビルド（[ビルド]メニュー → [ソリューションのリビルド]）が必要です。
+//
+// チュートリアル
+// https://siv3d.github.io/ja-jp/tutorial/tutorial/
+//
+// Tutorial
+// https://siv3d.github.io/tutorial/tutorial/
+//
+// Siv3D コミュニティへの参加（Discord などで気軽に質問や交流, 最新情報の入手ができます）
+// https://siv3d.github.io/ja-jp/community/community/
+//
+// Siv3D User Community
+// https://siv3d.github.io/community/community/
+//
+// 新機能の提案やバグの報告 | Feedback
+// https://siv3d.github.io/ja-jp/develop/report/
+//
+// Sponsoring Siv3D
+// https://github.com/sponsors/Reputeless
+//
---
+++ S2G/map_class.h
@@ -0,0 +1,148 @@
+﻿#pragma once
+#include <fstream>
+#include <sstream>
+
+class Map {
+	std::vector<std::vector<int>> map;
+	std::pair<int, int> player_pos = { 0, 0 };
+	std::pair<int, int> goal_pos = { 0, 0 };
+	std::vector<std::pair<int, int>> enemys_pos;
+	std::map<std::string, int> map_val = {
+		{".", 0},
+		{"#", 1},
+		{"s", 2},
+		{"g", 3},
+		{"e", 5},
+	};
+
+	void LoadMapCsv(std::string &path) {
+		std::ifstream csv(path);
+		if (not csv)
+			throw Error{ U"Failed to load Map csv" };
+
+		std::string line;
+		std::string map_data;
+		int i = 0;
+
+		while (std::getline(csv, line)) {
+			std::istringstream i_stream(line);
+			std::vector<int> m;
+			int j = 0;
+
+			while (std::getline(i_stream, map_data, ',')) {
+				if (map_data == "s") {
+					player_pos.first = i;
+					player_pos.second = j;
+				}
+
+				if (map_data == "g") {
+					goal_pos.first = i;
+					goal_pos.second = j;
+				}
+
+				if (map_data == "e") {
+					enemys_pos.push_back({ i, j });
+				}
+
+				m.push_back(map_val.at(map_data));
+				j++;
+			}
+			i++;
+			map.push_back(m);
+		}
+	}
+
+	bool CheckMove() {
+		int cell = map.at(player_pos.first).at(player_pos.second);
+		if (cell == map_val.at("#"))
+			return true;
+		return false;
+	}
+
+public:
+
+	Map(std::string& path){
+		LoadMapCsv(path);
+	}
+
+	bool CheckClear() {
+		if (map.at(player_pos.first).at(player_pos.second) == map_val.at("#"))
+			return true;
+		return false;
+	}
+
+	bool CheckOver() {
+		if (map.at(player_pos.first).at(player_pos.second) == map_val.at("e"))
+			return true;
+		return false;
+	}
+
+	void MovePlayer(int step, std::string dir) {
+		if (dir == "Left") {
+			player_pos.second -= 1;
+			if(CheckMove())
+				player_pos.second += 1;
+			DebugConsole();
+		}
+
+		if (dir == "Right") {
+			player_pos.second += 1;
+			if (CheckMove())
+				player_pos.second -= 1;
+			DebugConsole();
+		}
+
+		if (dir == "Up") {
+			player_pos.first -= 1;
+			if (CheckMove())
+				player_pos.first += 1;
+			DebugConsole();
+		}
+
+		if (dir == "Down") {
+			player_pos.first += 1;
+			if (CheckMove())
+				player_pos.first -= 1;
+			DebugConsole();
+		}
+	}
+
+	void DebugConsole() {
+		Print << player_pos.first;
+		Print << player_pos.second;
+		int i = 0;
+
+		for (auto vec : map) {
+			std::string str = "";
+			int j = 0;
+
+			for (auto s : vec) {
+				if (s == map_val.at("."))
+					str += " ";
+
+				if (s == map_val.at("#"))
+					str += "#";
+
+				if (s == map_val.at("s"))
+					str += " ";
+
+				if (s == map_val.at("g"))
+					str += "g";
+
+				if (s == map_val.at("e"))
+					str += "e";
+
+				if (i == player_pos.first && j == player_pos.second) {
+					str.pop_back();
+					str += "p";
+				}
+
+				j++;
+			}
+			const String text = Unicode::Widen(str);
+			Console << text;
+
+			i++;
+		}
+	}
+};
